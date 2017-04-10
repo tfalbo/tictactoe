cb20a00dfe154ab77b3cbd4640f1a99e523cbac9
Thaissa Falbo <thaissa.falbo@gmail.com>
2017-04-10 10:01:51 -0300

    Rascunho funcoes principais

---
+++ tictactoe.lua
@@ -0,0 +1,22 @@
+-- TicTacToe em Lua
+
+-- Cria vetor tabuleiro
+function createBoard()
+end
+
+-- Desenha tabuleiro
+function drawBoard()
+end
+
+-- Turno do jogador
+function play(piece)
+end
+
+-- Checa se alguem ganhou
+function checkWin()
+end
+
+-- Principal
+io.write("Começa o Jogo da Velha!\n\n")
+
+
