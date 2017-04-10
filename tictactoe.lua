01784e2b8c8a48451304a0a74cd7024b02c4d9a2
Thaissa Falbo <thaissa.falbo@gmail.com>
2017-04-10 11:14:26 -0300

    Turnos e jogadores ja colocam as pecas ok

--- tictactoe.lua
+++ tictactoe.lua
@@ -13,14 +13,39 @@ function createBoard()
 end
 
 -- Desenha tabuleiro
-function drawBoard(board)
+function drawBoard()
 	for i,v in ipairs(board) do
 		print(v)
 	end
 end
 
+
+-- Coloca a peca
+function placePiece(coordinate, piece)
+	if board[coordinate] == 0 then
+		board[coordinate] = piece
+		return true
+	else
+		return false
+	end
+end
+
+
 -- Turno do jogador
 function play(piece)
+	io.write(piece.. ", aqui está o tabuleiro:\n")
+	drawBoard()
+	local placed = false
+	while placed == false do
+		io.write("\nEscolha um lugar de 1 a 9 para colocar a peca\n")
+		local coordinate = tonumber(io.read())
+		placed = placePiece(coordinate, piece)
+		if placed == false then
+			io.write("Local já ocupado. Tente outro")
+		end
+	end
+	drawBoard()
+	io.write("\n")
 end
 
 -- Checa se alguem ganhou
@@ -30,5 +55,12 @@ end
 -- Principal
 io.write("Começa o Jogo da Velha!\n\n")
 createBoard()
-drawBoard(board)
+drawBoard()
 
+while true do
+	
+	play('x') 
+	play('o')
+
+end
+	
